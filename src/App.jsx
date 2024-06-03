d2469a4fcbf1398f9daba82bfe87b009a528f307
bruno bento <bdegan2016@gmail.com>
2024-06-02 23:57:48 -0400

    Update page title to "meu app"

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>React App</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        ola mundo
+      </p>
     </div>
   );
 }
